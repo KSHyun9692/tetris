57c7c2d1eab291233a33ba4e73049a75eb3b03fa
KSHyun9692 <KSHyun9692@gmail.com>
2021-11-04 22:57:24 +0900

    테트리스만들어보기

--- Tetris/js/tetris.js
+++ Tetris/js/tetris.js
@@ -12,7 +12,7 @@ let tempMovingItem;
 
 const BLOCKS = {
     tree:[
-        [[0,0],[0,1],[1,0],[1,1]],
+        [[2,1],[0,1],[1,0],[1,1]],
         [],
         [],
         [],
@@ -24,7 +24,6 @@ const movingItem = {
     direction: 0,
     top: 0,
     left: 0,
-
 };
 
 init();
@@ -32,10 +31,10 @@ init();
 //functions
 function init(){
     tempMovingItem = {...movingItem};
-
     for(let i=0; i<GAME_ROWS; i++){
         prependNewLine();
     }
+    renderBlocks();
 }
 
 function prependNewLine(){
@@ -49,5 +48,60 @@ function prependNewLine(){
     playground.prepend(li);
 }
 function renderBlocks(){
+    const {type, direction, top, left} = tempMovingItem;
+    const movingBlocks = document.querySelectorAll(".moving");
+    movingBlocks.forEach(moving=>{
+        moving.classList.remove(type, "moving");
+    });
+    BLOCKS[type][direction].forEach(block=>{
+        const x = block[0] + left;
+        const y = block[1] + top;
+        //console.log({playground});
+        const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null ;
+        const isAvailable = checkEmpty(target);
+        //console.log(target);
+        if(isAvailable){
+            target.classList.add(type, "moving");
+        }else{
+            tempMovingItem = {...movingItem};
+            // setTimeout(()=>{
+            //     renderBlocks();
+            // },0);
+            renderBlocks();
+        }
+    });
+    movingItem.left = left;
+    movingItem.top = top;
+    movingItem.direction = direction;
 
+    //console.log(type, direction, top, left);
 }
+function checkEmpty(target){
+    if(!target){
+        return false;
+    }
+    return true;
+}
+
+function moveBlock(moveType, amount){
+    tempMovingItem[moveType] += amount;
+    renderBlocks();
+}
+
+//event handling
+document.addEventListener("keydown", e => {
+    switch(e.keyCode){
+        case 39:
+            moveBlock("left",1);
+            break;
+        case 37:
+            moveBlock("left", -1);
+            break;
+        case 40:
+            moveBlock("top", 1);
+            break;
+        default:
+            break;
+    }
+    //console.log(e);
+});
